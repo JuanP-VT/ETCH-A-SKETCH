2634c91bc35d6d23817d8f8ec72203995508559b
PB-vt <cv.juanp@gmail.com>
2022-02-16 10:45:56 -0700

    Grid generator working

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const mainScreen = document.querySelector('#screen')
-const content =document.createElement('div');
-
-
-let numberOfGrids = prompt('select grid') **2
-
+//Generate a square grid with a prompt imput.
+const mainScreen = document.querySelector('#screen');
+let numberOfGrids = prompt('select grid') **2;
 function gridGenerator(cycle){
-    for (let index = 0; index <= cycle; index++) {
+    for (let index = 0; index < cycle; index++) {
         const newDiv =document.createElement('div');
         newDiv.className = 'pixel';
         mainScreen.appendChild(newDiv);
     
     }
 }
-
 let  gridValue = "";
 function gridTemplateColumValue(){
     const cycle = Math.sqrt(numberOfGrids);
     for (let index = 0; index < cycle; index++) {
         gridValue += ['auto ']
 }
+//Set *grid-template-columns* css property.
+let count = getComputedStyle(document.documentElement).getPropertyValue('--Template');
+document.documentElement.style.setProperty('--Template',gridValue)
+console.log(getComputedStyle(document.documentElement).getPropertyValue('--Template'));
 return gridValue;
 }
-//grid-template-columns has to be (auto )
+
 gridGenerator(numberOfGrids);
 gridTemplateColumValue();
+
